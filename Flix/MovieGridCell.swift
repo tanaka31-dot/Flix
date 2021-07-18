35b43f49e3273d79b737d9c74e6e9ddcc5b891a5
Tanaka Muchemwa <muchemwatanaka99@gmail.com>
2021-07-18 16:34:58 -0400

    updated

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Tanaka Muchemwa on 7/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+   
+    @IBOutlet weak var posterView: UIImageView!
+}
