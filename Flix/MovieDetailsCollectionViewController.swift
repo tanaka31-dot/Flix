c3714fa64fcd216f64c03225ae947f47ace1de3c
Tanaka Muchemwa <muchemwatanaka99@gmail.com>
2021-07-18 18:07:47 -0400

    bonus user stories

---
+++ Flix/MovieDetailsCollectionViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MovieDetailsCollectionViewController.swift
+//  Flix
+//
+//  Created by Tanaka Muchemwa on 7/18/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieDetailsCollectionViewController: UIViewController {
+    var movie: [String: Any]!
+
+    @IBOutlet weak var backdropView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.sizeToFit()
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+    
+        
+        posterView.af.setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+    
+        
+        backdropView.af.setImage(withURL: backdropUrl!)    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Flix is an app that allows users to browse movies from the [The Movie Database A
 - [x] (5pts) User can tap a tab bar button to view a grid layout of Movie Posters using a CollectionView.
 
 #### BONUS
-- [ ] (2pts) User can tap a poster in the collection view to see a detail screen of that movie.
+- [x] (2pts) User can tap a poster in the collection view to see a detail screen of that movie.
 - [ ] (2pts) In the detail view, when the user taps the poster, a new screen is presented modally where they can view the trailer.
 
 ### App Walkthrough GIF
@@ -19,6 +19,8 @@ Flix is an app that allows users to browse movies from the [The Movie Database A
 <img src="http://g.recordit.co/Pd7hehMBg9.gif" width=250><br>
 
 <img src="http://g.recordit.co/aSluJ6P98A.gif" width=250><br>
+<img src="http://g.recordit.co/TIkd1U8YrX.gif" width=250><br>
+
 ### Notes
 Describe any challenges encountered while building the app.
 ---
